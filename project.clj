8e268f3054ef4040b0a973cdd250d1ae001c6f84
Benjamin Zaporzan <benzaporzan@gmail.com>
2015-11-14 18:46:42 -0500

    added deps

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject find-the-invisible-beer "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/core.async "0.2.374"]
+                 [reagent "0.5.1"]]
+  :plugins [[lein-cljsbuild "1.1.1"]])
---
+++ test/find_the_invisible_beer/core_test.clj
@@ -0,0 +1,7 @@
+(ns find-the-invisible-beer.core-test
+  (:require [clojure.test :refer :all]
+            [find-the-invisible-beer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
